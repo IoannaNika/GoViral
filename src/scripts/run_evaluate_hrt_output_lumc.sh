7dedb82d345234cdd15c55304ac8fad3036d4657
Ioanna Nika <inika@login1.hpc.tudelft.nl>
2024-08-25 19:23:20 +0200

    fix output path

--- src/scripts/run_evaluate_hrt_output_lumc.sh
+++ src/scripts/run_evaluate_hrt_output_lumc.sh
@@ -1,30 +1,37 @@
 reference="../data/LUMC/ref/nCoV-2019.reference.fasta"
 results_path="/tudelft.net/staff-umbrella/ViralQuasispecies/inika/Benchmarking/src/results"
-output= "/tudelft.net/staff-umbrella/ViralQuasispecies/inika/Benchmarking/src/results/results.tsv"
+output="/tudelft.net/staff-umbrella/ViralQuasispecies/inika/Benchmarking/src/results/results.tsv"
 
 for sample in "01_100" "02_100" "03_50" "04_75" "05_90" "06_95" "07_98" "08_0" "09_0"; do   
 
     reads="/tudelft.net/staff-umbrella/ViralQuasispecies/inika/Read_simulators/data/lumc_data/natural_mixtures/${sample}/reads.tsv"
 
-    for ec_tool in "canu" "hifiasm" "lorma" "original"; do
+    for ec_tool in "original" "canu" "hifiasm" "lorma"; do
 
-        for hr_tool in "cliquesnv" "haplodmf" "rvhaplo"; do
+        for hr_tool in "haplodmf" "cliquesnv" "rvhaplo"; do
 
-            for mode in "whole_genome" "regions"; do
+            for mode in "per_region"; do
 
-                sample_name="{$sample}_{$ec_tool}_{$hr_tool}_{$mode}"
+                sample_name="${sample}-${ec_tool}-${hr_tool}-${mode}"
+                
+                echo ${sample_name}
+
                 if [[ "${mode}" == "whole_genome" ]]; then
-                    input="${results_path}/{$hr_tool}/{$ec_tool}/{$mode}/{$sample}/standard_output.tsv"
+                    input="${results_path}/${hr_tool}/${ec_tool}/${mode}/${sample}/standard_output.tsv"
                 else
-                    input="${results_path}/{$hr_tool}/{$ec_tool}/{$mode}/{$sample}/merged_standard_output.tsv"
+                    input="${results_path}/${hr_tool}/${ec_tool}/${mode}/${sample}/merged_standard_output.tsv"
                 fi
+
+                echo $input
 
                 # if the input file does not exist, skip
                 if [[ ! -f $input ]]; then
                     continue
                 fi
                 
-                python scripts/evaluate_hrt_output.py --input $input --ref_seq $reference --output $output --sample_name $sample_name --reads $reads
+                echo "file exists"
+
+                python -m scripts.evaluate_hrt_output_lumc --input $input --ref_seq $reference --output $output --sample_name $sample_name --reads $reads
             
             done
         done
