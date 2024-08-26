2aca4b5fd364cfe7ce2dafe34d410ba9f9609952
Ioanna Nika <inika@login1.hpc.tudelft.nl>
2024-08-26 21:21:49 +0200

    fix regions such that they are split correctly

--- src/scripts/standardise_output_tools.sh
+++ src/scripts/standardise_output_tools.sh
@@ -1,10 +1,4 @@
-genomic_regions="(54,1183),(1128,2244),(2179,3235),(3166,4240),(4189,5337),
-                    (5286,6358),(6307,7379),(7328,8363),(8282,9378),(9327,10429),
-                    (10370,11447),(11394,12538),(12473,13599),(13532,14619),
-                        (14568,15713),(15634,16698),(16647,17732),(17649,18684),
-                    (18618,19655),(19604,20676),(20581,21620),(21562,22590),
-                        (22537,23609),(23544,24714),(24658,25768),(25712,26835),
-                        (26766,27872),(27808,28985),(28699,29768),(29768,29790)"
+genomic_regions="(54,1183) (1128,2244) (2179,3235) (3166,4240) (4189,5337) (5286,6358) (6307,7379) (7328,8363) (8282,9378) (9327,10429) (10370,11447) (11394,12538) (12473,13599) (13532,14619) (14568,15713) (15634,16698) (16647,17732) (17649,18684) (18618,19655) (19604,20676) (20581,21620) (21562,22590) (22537,23609) (23544,24714) (24658,25768) (25712,26835) (26766,27872) (27808,28985) (28699,29768) (29768,29790)"
 
 reference="../data/LUMC/ref/nCoV-2019.reference.fasta"
 
@@ -18,11 +12,12 @@ for hrt_tool in "cliquesnv" "rvhaplo" "haplodmf"; do
             
             for region in $genomic_regions; do
                     
-                # remove the parentheses and split the region into start and end
+               # Remove the parentheses and split the region into start and end
                 region=$(echo $region | tr -d '()')
-                start=$(echo $region | cut -d ',' -f 1)
-                end_region=$(echo $region | cut -d ',' -f 2)
-                region_final="${start}_${end_region}"
+            
+                # Extract the first and last number using a comma as the delimiter
+                start=$(echo $region | cut -d',' -f1)
+                end_region=$(echo $region | cut -d',' -f2)
 
                 python -m scripts.standardise_output_tools_regions --results_dir results/ --ref_seq $reference --hrt $hrt_tool --ec $ec_tool --region $region_final --sample $sample
             
