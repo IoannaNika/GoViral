8e3cbde269732d91676e7aa93213a8c65cb6ea75
Ioanna Nika <inika@login1.hpc.tudelft.nl>
2024-09-01 14:21:38 +0200

    update data loader to accomodate existing training datasets

--- src/goViral/data_amplicon_reads.py
+++ src/goViral/data_amplicon_reads.py
@@ -10,16 +10,25 @@ class AmpliconReads(Dataset):
         self.transform = transform
         self.start = start
         self.test_mode = test_mode
+        
         self.reference_set = pd.read_csv(os.path.join(self.input_path), sep='\t', header=0, on_bad_lines='skip')
-        self.reference_set = self.reference_set[self.reference_set["start"] == int(self.start)]
+        
+        if self.test_mode: 
+            self.reference_set = self.reference_set[self.reference_set["start"] == int(self.start)]
+        
         self.length = len(self.reference_set)
 
     def __getitem__(self, index: int):
 
         items = self.reference_set.iloc[index]
 
-        id1 = items["id1"]
-        id2 = items["id2"]
+        if "id1" in self.reference_set.columns:
+            id1 = items["id1"]
+            id2 = items["id2"]
+        else:
+            id1 = items["read_1"]
+            id2 = items["read_2"]
+
 
         reads_dir = "/".join(self.input_path.split("/")[:-1])
         fasta_1_path = os.path.join(reads_dir, "reads",  '{}.fasta'.format(id1))
@@ -32,9 +41,8 @@ class AmpliconReads(Dataset):
         with open(fasta_2_path, 'r') as fasta_2_file:
             read2 = fasta_2_file.readlines()[1].strip()
         
-        gr = str(items["start"]) + "_" + str(items["end"])
-        
-        if not self.test_mode:
+
+        if not self.test_mode:            
             label = items["label"]
             target = 1 if label == 'positive' or label == 1 or label == "1" else 0
 
@@ -44,6 +52,8 @@ class AmpliconReads(Dataset):
             data = self.transform(data)
 
         if self.test_mode: 
+            gr = str(items["start"]) + "_" + str(items["end"])
+            
             return (id1, id2), data, gr
         else:
             return data, target
